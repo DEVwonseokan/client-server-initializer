160278990a4525b8433501fdec8be8e19143589a
DEVwonseokan <kargo95@gmail.com>
2022-10-06 23:11:05 +0900

    Initial commit

---
+++ src/main/java/com/devwon/clientserverinitializer/ClientServerInitializerApplication.java
@@ -0,0 +1,13 @@
+package com.devwon.clientserverinitializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientServerInitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientServerInitializerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devwon/clientserverinitializer/ClientServerInitializerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devwon.clientserverinitializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServerInitializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
